cc10181a30701b95261ce491633a874f1c751302
henriquelorenzini <38055951+henriquelorenzini@users.noreply.github.com>
2019-03-17 16:54:55 -0300

    Create Estacionamento-Update.sql

---
+++ Estacionamento-Update.sql
@@ -0,0 +1,15 @@
+UPDATE cliente  
+    SET Endereco = "RUA BLA BLA"
+    WHERE CPF = "1234567891";
+    
+UPDATE cliente  
+    SET Endereco = "RUA BLE BLE"
+    WHERE CPF = "0987654321";
+    
+    UPDATE cliente  
+    SET Endereco = "RUA BLI BLI"
+    WHERE CPF = "6754738291";
+    
+UPDATE cliente  
+    SET Endereco = "RUA BLU BLU"
+    WHERE CPF = "4378475738"
